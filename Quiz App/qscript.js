528a801429e2df66e2b3c250c1fe1caa8e5cd3d3
Sulphur10 <iamrajattripathi@gmail.com>
2022-12-22 10:57:12 +0530

    initial release

---
+++ Quiz App/qscript.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+    numb: 1,
+    question: "A pump can fill a tank with water in 2 hours. Because of a leak, it took 2 hours to fill the tank. The leak can drain all the water of the tank in:",
+    answer: "4.12 ",
+    options: [
+      "4.12",
+      "7",
+      "8",
+      "14"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Two pipes A and B can fill a cistern in 37 minutes and 45 minutes respectively. Both pipes are opened. The cistern will be filled in just half an hour, if the B is turned off after:?",
+    answer: "9 min",
+    options: [
+      "5 min",
+      "9 min",
+      "10 min",
+      "15 min"
+    ]
+  },
+    {
+    numb: 3,
+    question: "A tank is filled by three pipes with uniform flow. The first two pipes operating simultaneously fill the tank in the same time during which the tank is filled by the third pipe alone. The second pipe fills the tank 5 hours faster than the first pipe and 4 hours slower than the third pipe. The time required by the first pipe is:?",
+    answer: "15 hour",
+    options: [
+      "6 hour",
+      "10 hour",
+      "15 hour",
+      "25 hour"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the probability of getting an even number when a dice is thrown??",
+    answer: "1/2",
+    options: [
+      "1/2",
+      "1/4",
+      "1/6",
+      "1/8"
+    ]
+  },
+    {
+    numb: 5,
+    question: " The probability of getting two tails when two coins are tossed is -?",
+    answer: "1/4",
+    options: [
+      "1/2",
+      "1/4",
+      "1/9",
+      "1/5"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What is the probability of getting 1 and 5 if a dice is thrown once?",
+    answer: "1/3",
+    options: [
+      "1/8",
+      "1/3",
+      "1/7",
+      "1/4"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which of the following statement is true?",
+    answer: " Electrostatic force is a conservative force.",
+    options: [
+      " Electrostatic force is a conservative force.",
+      "Potential at a point is the work done per unit charge in bringing a charge from any point to infinity.",
+      "Electrostatic force is non-conservative",
+      " Potential is the productof charge and work."
+    ]
+  },
+  {
+    numb: 8,
+    question: "In a region of constant potential",
+    answer: "both (b) and (c) are correct.",
+    options: [
+      "the electric field is uniform.",
+      " the electric field is zero.",
+      "here can be no charge inside the region.",
+      "both (b) and (c) are correct."
+    ]
+  },
+  {
+    numb: 9,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 10,
+    question: "How many sizes of headers are available in HTML by default?",
+    answer: "6",
+    options: [
+      "1",
+      "4",
+      "6",
+      "7"
+    ]
+  },
+]
